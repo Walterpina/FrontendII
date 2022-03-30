d9b76d5df207fcc7701ea5ff7cc32653b0151c98
Walterpina <walterrpina@gmail.com>
2022-03-29 23:42:04 -0300

    Ajuste de pastas aula12 e add novo projeto de aula

---
+++ ToDo_modelo/README.md
@@ -0,0 +1,17 @@
+# CHecklist
+
+### HTML
+
+* Analisar index.html
+* ANalisar singup.html
+* Analisar tearefas.html
+
+### CSS
+
+* Analisar acesso.css
+* Analisar common.css
+* Analisar tarefas.css
+
+### JS
+
+* Criar javascripts das páginas html
--- aula12/script.js
+++ aula12/script.js
@@ -23,20 +23,22 @@ botaoInscrever.addEventListener("click", function (evento) {
     if (nomeValue.length > 1) {
       const invalido = nomeValue.find((palavra) => palavra.length === 1);
       if (invalido) {
-        console.log("entrei no inválido");
         // mensagem na tela e borda de erro no input
+        nome.classList.add("error");
         message.innerHTML = "O sobrenome está incompleto";
         message.style.display = "block";
         botaoInscrever.disabled = true;
       }
     } else {
       // precisa ser completo
+      nome.classList.add("error");
       message.innerHTML = "Preencha o nome e sobrenome";
       message.style.display = "block";
       botaoInscrever.disabled = true;
     }
   } else {
     // Limite de caracteres atingido
+    nome.classList.add("error");
     message.innerHTML = "Máximo de 150 caracteres.";
     message.style.display = "block";
     botaoInscrever.disabled = true;
@@ -53,8 +55,8 @@ const validaCampos = function () {
   
   //valida quantidade de campos checkbox hobbies
   if (checkHobbies() > 4) {
-    message.innerHTML =
-      "Desculpe, é preciso mais para ser super-homem... Continue tentando!!!";
+    console.log('entrei mais 4');
+    message.innerHTML = "Desculpe, é preciso mais para ser super-homem... Continue tentando!!!";
     message.style.display = "block";
     botaoInscrever.disabled = true;
   } else {
@@ -64,26 +66,27 @@ const validaCampos = function () {
 
   // valido a nacionalidade brasileira
   if (document.getElementById("nacionalidadeBrasileira").checked) {
-    message.innerHTML =
-      "Desculpe, ainda não estamos recrutando bruxos no Brasil, mas chegaremos em breve";
+    message.innerHTML = "Desculpe, ainda não estamos recrutando bruxos no Brasil, mas chegaremos em breve";
     message.style.display = "block";
     botaoInscrever.disabled = true;
   } else {
     botaoInscrever.disabled = false;
     message.style.display = "none";
   }
+
+  // validaChecks();
 };
 
 //contagem de check de hobbies
 const checkHobbies = function () {
+  console.log('contando...')
   let count = 0;
-  for (h of hobbies) {
-    if (h.checked) {
+  for (hobbie of hobbies) {
+    if (hobbie.checked) {
       count++;
     }
   }
   return count;
-  
 };
 
 // escuto a selecao do checkbox e valido a quantidade antes do click
@@ -93,4 +96,38 @@ hobbies.forEach((el) => el.addEventListener("change", validaCampos));
 nacionalidade.forEach((elemento) =>
   elemento.addEventListener("change", validaCampos)
 );
+
+function validaChecks() {
+  if(document.getElementById("hobbiesVideoGame").checked && document.getElementById("hobbiesCozinha").checked) {
+    message.innerHTML =
+    "Eu acho que algo queimará, para te ajudar nessa preciso que escolha algo diferente!!";
+    message.style.display = "block";
+    botaoInscrever.disabled = true;
+    return
+  } else {
+    botaoInscrever.disabled = false;
+    message.style.display = "none";
+  }
+
+  if(document.getElementById("hobbiesGuitarra").checked && document.getElementById("Leitura").checked) {
+    message.innerHTML =
+    "Ou música ou leitura... o que escolhe??";
+    message.style.display = "block";
+    botaoInscrever.disabled = true;
+    return
+  } else {
+    botaoInscrever.disabled = false;
+    message.style.display = "none";
+  }
+
+  if(document.getElementById("hobbiesNetflix").checked && document.getElementById("hobbiesCostura").checked) {
+    message.innerHTML = "Olhe o filme... você vai gostar!!!";
+    message.style.display = "block";
+    botaoInscrever.disabled = true;
+  } else {
+    botaoInscrever.disabled = false;
+    message.style.display = "none";
+  }
+
+}
 
